a6cda4d1473c3477e7933de4ede2218b3e319afd
Wahid Nory <wahid_kl@hotmail.com>
2014-09-15 14:25:43 +0200

    navigation drawer mayb

--- app/src/main/java/com/myhealth/application/activities/MainActivity.java
+++ app/src/main/java/com/myhealth/application/activities/MainActivity.java
@@ -3,15 +3,26 @@ package com.myhealth.application.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import com.myhealth.application.R;
 import com.myhealth.application.config.SessionManager;
 
 
 public class MainActivity extends Activity
 {
-    private SessionManager session;
+    private SessionManager  session;
+    private int             REQUEST_ENABLE_BT = 1;
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,8 +31,48 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
 
         session = new SessionManager(getApplicationContext());
-
         session.checkLogin();
+
+        mPlanetTitles = getResources().getStringArray(R.array.drawer_values);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.fragment_item_drawer, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id)
+            {
+
+            }
+
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle("hoi");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
     }
 
 
@@ -43,12 +94,12 @@ public class MainActivity extends Activity
             case R.id.action_settings:
                 //Als er op het settings icoon in de menubalk gedrukt is start dan de settings activiteit
                 Intent i = new Intent(this, SettingsActivity.class);
-                startActivity(i);
+                    startActivity(i);
                 return true;
             case R.id.action_search_bluetooth:
                 //Als er op de bluetooth zoek knop gedruk wordt moet er gezocht worden naar een bluetooth device
-                Intent j = new Intent(this, BluetoothActivity.class);
-                startActivity(j);
+                //Todo: code om naar bluetooth apparaat te zoeken
+                initBleutooth();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -56,4 +107,41 @@ public class MainActivity extends Activity
         }
     }
 
+    private void initBleutooth()
+    {
+//        //Activeer de bluetooth en kijk of de user bleutooth op zijn device heeft
+//        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//
+//        if (mBluetoothAdapter == null)
+//        {
+//            //User ondersteund geen Bleutooth
+//            Toast.makeText(getApplicationContext(), "No bleutooth support on your device", Toast.LENGTH_SHORT).show();
+//        }
+//
+//        //Als bluetooth uitstaat vraag de user om het aan te zetten
+//        if (!mBluetoothAdapter.isEnabled())
+//        {
+//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//        }
+//
+//        mBluetoothAdapter.startDiscovery();
+//        BroadcastReceiver mReceiver        = new BroadcastReceiver()
+//        {
+//            @Override
+//            public void onReceive(Context context, Intent intent)
+//            {
+//                String action = intent.getAction();
+//                // When discovery finds a device
+//                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                    // Get the BluetoothDevice object from the Intent
+//                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                    // Add the name and address to an array adapter to show in a ListView
+//                }
+//            }
+//        };
+//        // Register the BroadcastReceiver
+//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+//        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+    }
 }
